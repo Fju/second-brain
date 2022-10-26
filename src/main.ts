f8ee25c037aa665f4a176597ce083cf2733e9e64
Florian Winkler <flowi@outlook.com>
2022-10-26 20:18:35 +0200

    Setup environment with Vite, Vue, vue-router and TailwindCSS

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import './style.css'
+import App from './App.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: []
+})
+
+
+createApp(App)
+    .use(router)
+    .mount('#app')
